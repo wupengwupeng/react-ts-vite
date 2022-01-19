c58794238cd28be93345a34640637cabd96bc6fc
wupeng163 <wupenging163@163.com>
2022-01-19 10:40:20 +0800

    添加layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
-
+import BasicTabs from './nav/index'
 export function LayOut() {
   return (
     <div>
-      我是layout
+      {/* 我是layout
       <div>
         <Link to={"/"}>about</Link>
         <Link to={"/home"}>home</Link>
       </div>
-      <Outlet />
+      <Outlet /> */}
+      <BasicTabs />
     </div>
   );
 }
--- src/layout/nav/index.tsx
+++ src/layout/nav/index.tsx
@@ -1,7 +1,74 @@
-export function NavComponent(props:any){
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import { Outlet, Link, useNavigate } from "react-router-dom";
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+    // console.log(props);
+
     return (
-        <div>
-            我是和阿道尔
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography component={'div'} variant={'body2'}>{children}</Typography>
+                </Box>
+            )}
         </div>
-    )
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function BasicTabs() {
+    const [value, setValue] = React.useState(0);
+    const nevicate = useNavigate();
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+
+        setValue(newValue);
+        if (newValue === 1) {
+            nevicate('/home')
+        } else if (newValue === 2) {
+            nevicate('/about')
+        } else {
+            nevicate('/')
+        }
+    };
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="About" {...a11yProps(0)} />
+                    <Tab label="Home" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={value}>
+                <Outlet />
+            </TabPanel>
+        </Box>
+    );
 }
